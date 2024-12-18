f487c6292c57c0f452956e718e76023835cd50c7
GeekaN2 <geekan@bk.ru>
2024-12-18 19:52:19 +0300

    feat: add catcher class

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,8 +1,248 @@
-import { myUtil } from 'mylib-utils';
+import { HawkEvent, HawkNodeJSInitialSettings } from '../types';
+
+import {
+  EventContext,
+  AffectedUser,
+  EncodedIntegrationToken,
+  DecodedIntegrationToken,
+  EventData,
+  NodeJSAddons,
+  Json
+} from '@hawk.so/types';
+import { setJSExceptionHandler } from 'react-native-exception-handler';
+import EventPayload from './modules/event';
+
+class UnhandledRejection extends Error {}
+
+let _instance: Catcher;
+
+class Catcher {
+  private readonly type: string = 'errors/react-native';
+
+  private readonly token: EncodedIntegrationToken;
+
+  private readonly collectorEndpoint: string;
+
+  private readonly context?: EventContext;
+
+  private readonly beforeSend?: (event: EventData<NodeJSAddons>) => EventData<NodeJSAddons>;
+
+  constructor(settings: HawkNodeJSInitialSettings | string) {
+    if (typeof settings === 'string') {
+      settings = {
+        token: settings,
+      } as HawkNodeJSInitialSettings;
+    }
+
+    this.token = settings.token;
+    this.context = settings.context || undefined;
+    this.beforeSend = settings.beforeSend;
+
+    if (!this.token) {
+      throw new Error('Integration Token is missed. You can get it on https://hawk.so at Project Settings.');
+    }
+
+    try {
+      const integrationId = this.getIntegrationId();
+
+      this.collectorEndpoint = settings.collectorEndpoint || `https://${integrationId}.k1.hawk.so/`;
+
+      /**
+       * Set global handlers
+       */
+      if (!settings.disableGlobalErrorsHandling) {
+        this.initGlobalHandlers();
+      }
+    } catch (error) {
+      throw new Error('Invalid integration token');
+    }
+  }
+
+  /**
+   * Catcher package version
+   */
+  private static getVersion(): string {
+    // TODO: get version from package.json
+    return '1.0.0';
+  }
+
+  /**
+   * Send test event from client
+   */
+  public test(): void {
+    /**
+     * Create a dummy error event
+     * Error: Hawk NodeJS Catcher test message
+     */
+    const fakeEvent = new Error('Hawk NodeJS Catcher test message');
+
+    /**
+     * Catch it and send to Hawk
+     */
+    this.send(fakeEvent);
+  }
+
+  /**
+   * This method prepares and sends an Error to Hawk
+   * User can fire it manually on try-catch
+   *
+   * @param error - error to catch
+   * @param context — event context
+   * @param user - User identifier
+   */
+  public send(error: Error, context?: EventContext, user?: AffectedUser): void {
+    /**
+     * Compose and send a request to Hawk
+     */
+    this.formatAndSend(error, context, user);
+  }
+
+  /**
+   * Returns integration id from integration token
+   */
+  private getIntegrationId(): string {
+    const decodedIntegrationTokenAsString = Buffer
+      .from(this.token, 'base64')
+      .toString('utf-8');
+    const decodedIntegrationToken: DecodedIntegrationToken = JSON.parse(decodedIntegrationTokenAsString);
+    const integrationId = decodedIntegrationToken.integrationId;
+
+    if (!integrationId || integrationId === '') {
+      throw new Error('Invalid integration token. There is no integration ID.');
+    }
+
+    return integrationId;
+  }
+
+  /**
+   * Define own error handlers
+   */
+  private initGlobalHandlers(): void {
+    setJSExceptionHandler((error, isFatal) => {
+      console.log('Error', error, error.stack, isFatal);
+
+      this.sendErrorFormatted({
+        catcherType: this.type,
+        payload: {
+          title: error.name,
+        },
+        token: this.token,
+      })
+    }, true);
+  };
+
+  /**
+   * Format and send an error
+   *
+   * @param {Error} err - error to send
+   * @param {EventContext} context — event context
+   * @param {AffectedUser} user - User identifier
+   */
+  private formatAndSend(err: Error, context?: EventContext, user?: AffectedUser): void {
+    const eventPayload = new EventPayload(err);
+    let payload: EventData<NodeJSAddons> = {
+      title: eventPayload.getTitle(),
+      type: eventPayload.getType(),
+      backtrace: eventPayload.getBacktrace(),
+      user: this.getUser(user),
+      context: this.getContext(context),
+      catcherVersion: Catcher.getVersion(),
+    };
+
+    /**
+     * Filter sensitive data
+     */
+    if (typeof this.beforeSend === 'function') {
+      payload = this.beforeSend(payload);
+    }
+
+    this.sendErrorFormatted({
+      token: this.token,
+      catcherType: this.type,
+      payload,
+    });
+  }
+
+  /**
+   * Sends formatted EventData<NodeJSAddons> to the Collector
+   *
+   * @param {EventData<NodeJSAddons>>} eventFormatted - formatted event to send
+   */
+  private async sendErrorFormatted(eventFormatted: HawkEvent): Promise<void | Response> {
+    return fetch(this.collectorEndpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(eventFormatted),
+    })
+      .catch((err: Error) => {
+        console.error(`[Hawk] Cannot send an event because of ${err.toString()}`);
+      });
+  }
+
+  /**
+   * Compose User object
+   *
+   * @param user - User identifier
+   */
+  private getUser(user?: AffectedUser): AffectedUser|undefined {
+    return user;
+  }
+
+  /**
+   * Compose context object
+   *
+   * @param context - Any other information to send with event
+   */
+  private getContext(context?: EventContext): Json {
+    const contextMerged = {};
+
+    if (this.context !== undefined) {
+      Object.assign(contextMerged, this.context);
+    }
+
+    if (context !== undefined) {
+      Object.assign(contextMerged, context);
+    }
+
+    return contextMerged;
+  }
+}
 
 /**
- * Simple function that returns string
+ * Wrapper for Hawk NodeJS Catcher
  */
-export default function main(): string {
-  return 'It works! ' + myUtil(1, 2);
+export default class HawkCatcher {
+  /**
+   * Wrapper for HawkCatcher constructor
+   *
+   * @param settings - If settings is a string, it means an Integration Token
+   */
+  public static init(settings: HawkNodeJSInitialSettings | string): void {
+    _instance = new Catcher(settings);
+  }
+
+  /**
+   * Wrapper for HawkCatcher.send() method
+   *
+   * This method prepares and sends an Error to Hawk
+   * User can fire it manually on try-catch
+   *
+   * @param error - error to catch
+   * @param context — event context
+   * @param user - User identifier
+   */
+  public static send(error: Error, context?: EventContext, user?: AffectedUser): void {
+    /**
+     * If instance is undefined then do nothing
+     */
+    if (_instance) {
+      return _instance.send(error, context, user);
+    }
+  }
 }
+
+export {
+  HawkNodeJSInitialSettings
+};
---
+++ packages/core/src/modules/backtrace.ts
@@ -0,0 +1,148 @@
+import { BacktraceFrame, SourceCodeLine } from '@hawk.so/types';
+import fs from 'fs';
+import stackTrace, { StackFrame } from 'stack-trace';
+
+/**
+ * Number of file lines before and after errored line
+ * to be read and send to Hawk for better event view
+ */
+const LINES_BUFFER = 8;
+
+/**
+ * Parsing and formatting error's backtrace
+ */
+export default class BacktraceHelper {
+  /**
+   * Error to be processed
+   */
+  private error: Error;
+
+  /**
+   * Initialize a class
+   *
+   * @param {Error} error — event error to be processed
+   */
+  constructor(error: Error) {
+    this.error = error;
+  }
+
+  /**
+   * Return backtrace matching types scheme
+   *
+   * @returns {BacktraceFrame[]}
+   */
+  public getBacktrace(): BacktraceFrame[] {
+    if (!this.error) {
+      return [];
+    }
+
+    /**
+     * Get stack frames
+     */
+    const parsedStack = stackTrace.parse(this.error);
+
+    /**
+     * Format frame data and gather them to array
+     */
+    const parsedFrames = parsedStack.map(this.parseStackFrame.bind(this));
+
+    /**
+     * Filter backtrace frames
+     * Remove calls with a missing source code (internal calls)
+     */
+    return parsedFrames.filter((frame: BacktraceFrame) => frame.sourceCode);
+  }
+
+  /**
+   * Format frame data to BacktraceFrame scheme
+   *
+   * @param {StackTraceFrame} frame — backtrace step
+   * @returns {BacktraceFrame}
+   */
+  private parseStackFrame(frame: StackFrame): BacktraceFrame {
+    /** Create variable for sourceCode data */
+    let sourceCode: SourceCodeLine[] = [];
+
+    /**
+     * Get source file for non-internal call frames
+     * Check if we need to try to reach sources and show code pieces
+     */
+    if (!this.isInternal(frame) && frame.getFileName() && frame.getLineNumber()) {
+      sourceCode = this.getFileChunk(frame.getFileName(), frame.getLineNumber());
+    }
+
+    /** Push backtrace frame */
+    return {
+      file: frame.getFileName(),
+      line: frame.getLineNumber(),
+      column: frame.getColumnNumber(),
+      function: frame.getFunctionName(),
+      sourceCode: sourceCode,
+    };
+  }
+
+  /**
+   * Check if frame is an internal node call
+   * You should not reach sources and show code pieces for internal calls
+   *
+   * @param frame — backtrace step
+   * @returns {boolean}
+   */
+  private isInternal(frame: StackFrame): boolean {
+    const hasEntriesForNonInternalCalls = frame.getFileName() &&
+      frame.getFileName().indexOf(':\\') !== 1 &&
+      !frame.getFileName().startsWith('.') &&
+      !frame.getFileName().startsWith('/');
+
+    return frame.isNative() || !!hasEntriesForNonInternalCalls;
+  }
+
+  /**
+   * Read a source file as an array of lines
+   *
+   * @param {string} filepath - path to file to be read
+   * @returns {string[]}
+   */
+  private getSourceFileAsLines(filepath: string): string[] {
+    if (!fs.existsSync(filepath)) {
+      return [];
+    }
+
+    return fs.readFileSync(filepath, 'utf-8')
+      .split('\n');
+  }
+
+  /**
+   * Return lines as file chuck with a target line
+   *
+   * @param {string} filename - path to file to be read
+   * @param {number} targetLine - number of line with an call
+   * @param {number} linesBuffer — number of lines before and after target line to be read
+   */
+  private getFileChunk(filename: string, targetLine: number, linesBuffer: number = LINES_BUFFER): SourceCodeLine[] {
+    /**
+     * Read source file as array of lines
+     */
+    const lines = this.getSourceFileAsLines(filename);
+
+    /** Count line number in an array */
+    const actualLineNumber = targetLine - 1; // starts from 0;
+
+    /** Define line number to start copy code */
+    const lineFrom = Math.max(0, actualLineNumber - linesBuffer);
+
+    /** Define line number to end copy code */
+    const lineTo = Math.min(lines.length - 1, actualLineNumber + linesBuffer + 1);
+
+    /** Get buffer lines from files */
+    const linesToCollect = lines.slice(lineFrom, lineTo);
+
+    /** Compose code chunk of file */
+    return linesToCollect.map((content: string, index: number): SourceCodeLine => {
+      return {
+        line: lineFrom + index + 1,
+        content,
+      };
+    });
+  }
+}
